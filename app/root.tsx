5ea1be69ab6dc6af2199546d9ad77fde51ae42c9
Nanda Kancharla <nanda.k@fastpix.io>
2024-05-26 03:43:58 +0530

    Update root.tsx

--- app/root.tsx
+++ app/root.tsx
@@ -16,6 +16,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
+        <h1>Hey How have you been?</h1>
         {children}
         <ScrollRestoration />
         <Scripts />
